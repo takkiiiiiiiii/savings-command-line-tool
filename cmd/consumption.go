624064ff112e0a62c382ddc826fc3268470340dc
takkiiiiiiiii <yudai2002brazil@gmail.com>
2023-02-27 15:51:06 +0900

    add consumption.go

---
+++ cmd/consumption.go
@@ -0,0 +1,49 @@
+package cmd
+
+type ConsumptionAll struct {
+	total             int // 今まで支出した合計
+	year_index        int
+	comsumptionByYear []ConsumptionByYear
+}
+
+type ConsumptionByYear struct {
+	total              int //　年の支出の合計
+	consumptionByMonth [12]ConsumptionByMonth
+}
+
+type ConsumptionByMonth struct {
+	total            int // 月の支出の合計
+	consumptionByDay [31]int
+}
+
+func (c *ConsumptionAll) conusmptionAll() int {
+	return c.total
+}
+
+func (c *ConsumptionAll) consumption(todayConsumption, month, day int) (int, int, int) {
+	consumptionOfMonth := c.consumptionFromTheYearFirstDayToToday(todayConsumption, month, day)
+	consumptionOfDay := c.dayOfConsumption(todayConsumption, month, day)
+    consumptionOfYear := c.consumptionFromTheYearFirstDayToToday(todayConsumption, month, day)
+	return consumptionOfYear, consumptionOfMonth, consumptionOfDay
+}
+
+// 年の支出の合計を取得
+func (c *ConsumptionAll) consumptionFromTheYearFirstDayToToday(todayConsumption, month, day int) int {
+	c.comsumptionByYear[c.year_index].total += todayConsumption
+	if month == 12 && day == 31 {
+		c.year_index++
+	}
+	return c.comsumptionByYear[c.year_index].total
+}
+
+// 月の支出の合計を取得
+func (c *ConsumptionAll) consumptionFromTheMonthFirstDayToToday(todayConsumption, month int) int {
+	c.comsumptionByYear[c.year_index].consumptionByMonth[month-1].total += todayConsumption
+	return c.comsumptionByYear[c.year_index].consumptionByMonth[month-1].total
+}
+
+// 日の支出を保存
+func (c *ConsumptionAll) dayOfConsumption(todayConsumption, month, day int) int {
+	c.comsumptionByYear[c.year_index].consumptionByMonth[month-1].consumptionByDay[day-1] += todayConsumption
+	return c.comsumptionByYear[c.year_index].consumptionByMonth[month-1].consumptionByDay[day-1]
+}
--- cmd/saving.go
+++ cmd/saving.go
@@ -20,21 +20,8 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		now := time.Now()
-		/*
-			year, month, day := time.Now().Date()
-			month_name := month.String()
-			fileByMonth := month_name + ".go"
-			file, err := os.OpenFile("fileByMonth", os.O_RDWR|os.O_CREATE, 0755)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if err := f.Close(); err != nil {
-				log.Fatal(err)
-			}
-		*/
-		// todayConsumption, _ := strconv.Atoi(args[0])
+
 		fmt.Printf("%d年%d月%d日 使用金額 : %s\n", now.Year(), int(now.Month()), now.Day(), args[0])
-		// _, err := fmt.Fprintln(file, todayConsumption)
 	},
 }
 
