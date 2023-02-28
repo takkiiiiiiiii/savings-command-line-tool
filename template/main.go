41775b8cabfd44ef8c3c796b24ef12ef38cf1be7
takkiiiiiiiii <yudai2002brazil@gmail.com>
2023-02-28 12:31:59 +0900

    To save input value

--- template/main.go
+++ template/main.go
@@ -1,5 +1,45 @@
 package template
 
-func SavingTemplate() {
+import (
+	"fmt"
+)
+
+type ConsumptionAll struct {
+	total             int // 今まで支出した合計
+	year_index        int
+	comsumptionByYear []ConsumptionByYear
+}
+
+type ConsumptionByYear struct {
+	total              int //　年の支出の合計
+	consumptionByMonth [12]ConsumptionByMonth
+}
+
+type ConsumptionByMonth struct {
+	total            int // 月の支出の合計
+	consumptionByDay [31]int
+}
+
+var consumptionByMonth = &ConsumptionByMonth{
+	total:            0,
+	consumptionByDay: [31]int{},
+}
+
+var consumptionByYear = &ConsumptionByYear{
+	total:              0,
+	consumptionByMonth: [12]ConsumptionByMonth{},
+}
+
+var consumptionAll = &ConsumptionAll{
+	total:             0,
+	year_index:        0,
+	comsumptionByYear: []ConsumptionByYear{},
+}
+
+func addConsumption(consumptionAll ConsumptionAll) {
+
+}
+
+func main() {
 
 }
